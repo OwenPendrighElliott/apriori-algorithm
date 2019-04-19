931069de9196cc54186d46fd4a632fa11c71c41c
Owen Elliott <u5788450@anu.edu.au>
2019-04-19 14:07:00 +1000

    Added Comments

--- Apriori.py
+++ Apriori.py
@@ -4,6 +4,11 @@ import time
 
 
 def load_csv(filename):
+    """
+    Load a csv file and represent it as a list of transactions also appending the column name to each value.
+    :param filename: The name of the file which is placed in the DATA folder
+    :return: The data as a list of transactions with the column name appended to each value in the column
+    """
     df = pd.read_csv("DATA\\" + filename)
     names = list(df.columns.values)[1:]
     dl = list(df.values.tolist())
@@ -16,6 +21,14 @@ def load_csv(filename):
 
 
 def apriori(data, min_sup, min_conf):
+    """
+    apriori runs all the parts needed to generate the association rules and passes the values between them.
+    :param data: The data as a list of transactions
+    :param min_sup: The minimum support
+    :param min_conf: The minimum confidence
+    :return: A list of tuples containing the information for each association rule.
+             (left hand side, right hand side, support, confidence)
+    """
     set_supports = find_freq(data, min_sup)
     print_itemsets(set_supports)
     assoc_rules = apriori_gen(set_supports, min_conf)
@@ -24,10 +37,19 @@ def apriori(data, min_sup, min_conf):
 
 
 def apriori_gen(set_supports, min_conf):
+    """
+    Generate the association rules.
+    :param set_supports: A dictionary where the key is the itemset and the value is its support.
+    :param min_conf: The minimum confidence
+    :return: A list of tuples containing the information for each association rule.
+             (left hand side, right hand side, support, confidence)
+    """
     max_len = 0
     assoc_itemsets = {}
+    # Find the longest itemset
     for key in set_supports:
         max_len = len(key)
+    # Make a list of only frequent itemsets shorter than the longest itemsets
     for key in set_supports:
         if len(key) != max_len:
             assoc_itemsets[key] = set_supports[key]
@@ -35,8 +57,11 @@ def apriori_gen(set_supports, min_conf):
     assoc_rules = []
     for lhs in assoc_itemsets:
         for rhs in assoc_itemsets:
-            if not lhs.issubset(rhs) and not rhs.issubset(lhs) and not any(x in lhs for x in rhs):
+            # no item in the left side is also in the right side
+            if not any(item in lhs for item in rhs):
                 superset = lhs.union(rhs)
+                # the set of the left and right hand sides is shorter than the longest frequent itemsets and both the
+                # superset and the left side exist in the list of frequent itemsets.
                 if len(superset) <= max_len and superset in set_supports and lhs in set_supports:
                     confidence = set_supports[superset]/set_supports[lhs]
                     if confidence > min_conf:
@@ -45,8 +70,19 @@ def apriori_gen(set_supports, min_conf):
 
 
 def find_freq(data, min_sup):
+    """
+    Find all frequent itemsets using initial_freq to first find all frequent one item itemsets and then using
+    get_support to find the support for each itemset.
+    :param data: The data as a list of transactions
+    :param min_sup: The minimum support
+    :return: A dictionary where the key is the itemset and the value is its support
+    """
+    # Find the one item itemsets
     initial_items = initial_freq(data, min_sup)
+
     supersets = []
+    # generate all combinations of the frequent one item itemsets
+    
     for i in range(1, len(initial_items)):
         combinations = itertools.combinations(list(initial_items), i)
         for c in combinations:
@@ -56,6 +92,13 @@ def find_freq(data, min_sup):
 
 
 def get_support(data, sets, min_sup):
+    """
+    calculate support for all frequent itemsets.
+    :param data: The data as a list of transactions
+    :param sets: All the frequent itemsets
+    :param min_sup: The minimum support
+    :return: A dictionary where the key is the itemset and the value is its support
+    """
     set_support = {}
     for s in sets:
         count = 0
@@ -69,6 +112,12 @@ def get_support(data, sets, min_sup):
 
 
 def initial_freq(data, min_sup):
+    """
+    Find the one item itemsets.
+    :param data: The data as a list of transactions
+    :param min_sup: The minimum support
+    :return: A dictionary where with key is the single item itemset and the value is its support
+    """
     v_c = {}
     for tran in data:
         for item in tran:
@@ -87,6 +136,11 @@ def initial_freq(data, min_sup):
 
 
 def print_itemsets(set_supports):
+    """
+    Prints out the frequent itemsets to make them look pretty.
+    :param set_supports: A dictionary where the key is the itemset and the value is its support
+    :return: None
+    """
     print("The following " + str(len(set_supports)) + " frequent itemsets were found:")
     for itemset in set_supports:
         print(str(itemset) + ": support = " + str(set_supports[itemset]))
@@ -94,6 +148,12 @@ def print_itemsets(set_supports):
 
 
 def print_rules(assoc_rules):
+    """
+    Prints out the association rules to make them look pretty.
+    :param assoc_rules: A list of tuples containing the information for each association rule.
+                        (left hand side, right hand side, support, confidence)
+    :return: None
+    """
     print("The following " + str(len(assoc_rules)) + " rules that satisfy min_sup and min_conf are generated: ")
     for rule in assoc_rules:
         print(str(rule[0]) + " -> " + str(rule[1]) + " | support: " +
@@ -104,10 +164,8 @@ def print_rules(assoc_rules):
 def main():
     start = time.time()
 
-    # dl = load_csv("pokemon.csv")
-    # dl = load_csv("stock_prices_2018.csv")
-    dl = load_csv("fifa_player_data.csv")
-    # dl = [['a', 'b', 'c', 'e', 'f'], ['a', 'b', 'c', 'd', 'f']]
+    dl = load_csv("my_data.csv")
+
     min_sup = 0.3
     min_conf = 0.6
     print("min_sup = " + str(min_sup))
